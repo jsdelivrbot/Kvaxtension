a8fb0c32fd98b94b8011dc40e4d6076b31fe813f
kvackkvack <friskarthur@gmail.com>
2015-12-17 15:42:45 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,29 @@
 (function(ext) {
-    ext._shutdown = function() {
-    };
-    ext._getStatus = function() {
-      return {status: 2, msg: 'Thanks for installing!'};
-    };
-    ext.installed = function() {
-    	return(true);
-    };
+  ext._shutdown = function() {
+  };
+  ext._getStatus = function() {
+    return {status: 2, msg: 'Thanks for installing!'};
+  };
+  ext.installed = function() {
+  	return(true);
+  };
 
-    var descriptor = {
-        blocks: [ ], menus: { }
-      };
- 	}
+  var descriptor = {
+    blocks: [], menus: {}
+  };
 
   var addBlock = function(block) {
     descriptor.blocks.push(block);
+  }
+
+  var refreshExt = function() {
+    ScratchExtensions.unregister('kvaxtension: Test...');
+    ScratchExtensions.register('kvaxtension: Test...', descriptor, ext);
   }
 
   addBlock(['R', 'test!', 'test'])
   var test = function() {
     return 'test!'
   }
-  ScratchExtensions.register("kvaxtension: Test...", descriptor, ext);
+  refreshExt();
 })({});
