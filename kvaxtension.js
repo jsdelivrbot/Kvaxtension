e567fa2d8822c162e13da364bc8b529b8017e838
kvackkvack <friskarthur@gmail.com>
2015-12-17 15:16:48 +0100

    Update kvaxtension.js

--- kvaxtension.js
+++ kvaxtension.js
@@ -1,1 +1,2 @@
 ScratchExtensions.loadExternalJS("https://rawgit.com/kvackkvack/Kvaxtension/master/general.js");
+ScratchExtensions.loadExternalJS("https://rawgit.com/kvackkvack/Kvaxtension/master/test.js");
